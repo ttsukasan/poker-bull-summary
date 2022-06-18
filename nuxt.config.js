34390a6720d84e1d89800dea861a33631931699a
t.tsukamoto <tmak.tsukamoto@gmail.com>
2022-06-19 02:25:35 +0900

    boxiconsを導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    'boxicons/css/boxicons.min.css'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
