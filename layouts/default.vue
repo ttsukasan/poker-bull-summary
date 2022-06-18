e3123f9312a34d808eb36fce5a144ca2ee382391
t.tsukamoto <tmak.tsukamoto@gmail.com>
2022-06-19 02:13:02 +0900

    レイアウトの雛形を作成

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<!-- 基本レイアウト -->
+<template>
+  <div>
+    <!-- ヘッダー -->
+    <Header></Header>
+    <!-- メイン -->
+    <Nuxt />
+    <!-- フッター -->
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
