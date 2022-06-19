28b885f87a64ef63540173015a7bde48ebc9a2aa
t.tsukamoto <tmak.tsukamoto@gmail.com>
2022-06-20 00:29:35 +0900

    ヘッダーの修正 非透過にする

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav id="header" class="fixed w-full z-10 top-0">
+  <nav id="header" class="fixed w-full z-10 top-0 bg-white shadow">
     <div class="w-full md:max-w-4xl mx-auto flex flex-wrap items-center justify-between mt-0 py-3">
       <!-- タイトル -->
       <div class="pl-4">
